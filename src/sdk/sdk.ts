3c61766784ef19a382c7596dbbc0717cb4cf847a
Speakeasy CI <ci@speakeasyapi.dev>
2023-01-04 00:11:35 +0000

    ci: regenerated with OpenAPI Doc 0.1.0, Speakeay CLI 0.17.2

--- src/sdk/sdk.ts
+++ src/sdk/sdk.ts
@@ -39,8 +39,8 @@ export class SDK {
   public _securityClient: AxiosInstance;
   public _serverURL: string;
   private _language = "typescript";
-  private _sdkVersion = "0.6.1";
-  private _genVersion = "0.16.1";
+  private _sdkVersion = "0.7.0";
+  private _genVersion = "0.17.2";
 
   constructor(props: SDKProps) {
     this._serverURL = props.serverUrl ?? ServerList[ServerProd];
